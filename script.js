4ea542a2a72e5a7d6b47e66048dffc6801aaaa98
Duadze00 <duadze00@gmail.com>
2025-10-25 00:10:57 -0700

    js removed

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-let userName = prompt("What's your name: ");
-console.log(`Your name is ${userName}`);
-if (userName === "Eric") {
-  console.log(true);
-} else {
-  console.log(false);
-}
-function greet() {
-  let x = parseInt(prompt("Enter x value: "));
-  let y = parseInt(prompt("Enter y value: "));
-  return x + y;
-}
+
